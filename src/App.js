e3fb4d55a44be3a3a6554b04597ca5bd0b7d8ecb
Shubham <yesshubham@gmail.com>
2022-11-24 13:00:57 -0800

    chore: add react-big-calendar and moment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import Button from '@mui/material/Button';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
 
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment);
+
+const MyCalendar = (props) => (
+  <div>
+    <Calendar
+      localizer={localizer}
+      events={[
+        {
+          start: moment().toDate(),
+          end: moment().add(1, 'days').toDate(),
+          title: 'Some title'
+        }
+      ]}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 500 }}
+    />
+  </div>
+);
 const App = () => {
-  return <Button variant="contained">Contained</Button>;
+  return (
+    <div>
+      <Button variant="contained">Contained</Button>
+      <MyCalendar />
+    </div>
+  );
 };
 
 export default App;
