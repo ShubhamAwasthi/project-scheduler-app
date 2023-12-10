ab69c1ec6a4031de0c12959b8d98c4e2ef094756
Shubham <yesshubham@gmail.com>
2023-12-09 20:34:27 -0800

    feat: collect workers eligible for tasks

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,7 @@
+/* eslint-disable guard-for-in */
 import * as Lockr from 'lockr';
 import { PROJECTS_KEY } from '../constants';
+import * as moment from 'moment';
 
 export const setProjects = (projects) => {
   console.log('before saving to browser', projects);
@@ -12,7 +14,7 @@ export const getProjects = () => {
 };
 
 export const assignTasks = (project) => {
-  console.log(`before assigning tasks ${project}`);
+  console.log(`before assigning tasks`, project);
   /**
    * Algorithm
    * iterate tasks in the order of id
@@ -28,6 +30,189 @@ export const assignTasks = (project) => {
    * add the worker detail to this task
    * once all tasks are assigned we can return the project
    */
-  console.log(`after assigning tasks ${project}`);
+  // Iterate the tasks
+  const projectStartDateMoment = moment(project.startDate);
+  const weekendDays = [0, 7];
+  for (const task of project.tasks) {
+    const nextWorkingDay = getNextWorkingDay(projectStartDateMoment, weekendDays, project.holidays);
+    console.log(nextWorkingDay, task);
+    const skillWorkerMap = {};
+    for (const skill of task.skills) {
+      if (!skillWorkerMap[skill.level]) {
+        skillWorkerMap[skill.level] = [];
+      }
+      for (const worker of project.workers) {
+        if (worker.skills.some((x) => x.level == skill.level && x.count > 0)) {
+          skillWorkerMap[skill.level].push(worker);
+        }
+      }
+      if (skill.count > skillWorkerMap[skill.level].length) {
+        continue;
+      }
+    }
+    console.log(`skillMap: `, skillWorkerMap, task);
+  }
+  console.log(`after assigning tasks`, project);
   return project;
 };
+
+const getNextWorkingDay = (projectStartDateMoment, weekendDays, holidays) => {
+  console.log(`Before calculating next working day`, projectStartDateMoment, weekendDays, holidays);
+  while (
+    weekendDays.includes(projectStartDateMoment.day()) ||
+    holidays.some((holiday) =>
+      projectStartDateMoment.isBetween(holiday.startDate, holiday.endDate, 'days', '[]')
+    )
+  ) {
+    projectStartDateMoment = projectStartDateMoment.add(1, 'd');
+  }
+  console.log(`After calculating next working day`, projectStartDateMoment, weekendDays, holidays);
+  return projectStartDateMoment;
+};
+
+// Refer below structure of project
+// {
+//     "id": 1702178465838,
+//     "name": "CalculatorApp",
+//     "startDate": "2023-11-16T05:25:00.000Z",
+//     "tasks": [
+//         {
+//             "id": 1699421196376,
+//             "name": "FrontEnd",
+//             "skills": [
+//                 {
+//                     "level": "SKILL_HIGH",
+//                     "count": 0
+//                 },
+//                 {
+//                     "level": "SKILL_MEDIUM",
+//                     "count": 1
+//                 },
+//                 {
+//                     "level": "SKILL_LOW",
+//                     "count": 0
+//                 }
+//             ],
+//             "days": "5",
+//             "workers": []
+//         },
+//         {
+//             "id": 1699421225906,
+//             "name": "DomainLogic",
+//             "skills": [
+//                 {
+//                     "level": "SKILL_HIGH",
+//                     "count": 1
+//                 },
+//                 {
+//                     "level": "SKILL_MEDIUM",
+//                     "count": 1
+//                 },
+//                 {
+//                     "level": "SKILL_LOW",
+//                     "count": 0
+//                 }
+//             ],
+//             "days": "3",
+//             "workers": []
+//         },
+//         {
+//             "id": 1699421332290,
+//             "name": "ThemeCreation",
+//             "skills": [
+//                 {
+//                     "level": "SKILL_HIGH",
+//                     "count": 0
+//                 },
+//                 {
+//                     "level": "SKILL_MEDIUM",
+//                     "count": 1
+//                 },
+//                 {
+//                     "level": "SKILL_LOW",
+//                     "count": 0
+//                 }
+//             ],
+//             "days": "5",
+//             "workers": []
+//         }
+//     ],
+//     "workers": [
+//         {
+//             "id": 1699421368835,
+//             "name": "Shubham",
+//             "skills": [
+//                 {
+//                     "level": "SKILL_HIGH",
+//                     "count": 1
+//                 },
+//                 {
+//                     "level": "SKILL_MEDIUM",
+//                     "count": 0
+//                 },
+//                 {
+//                     "level": "SKILL_LOW",
+//                     "count": 0
+//                 }
+//             ]
+//         },
+//         {
+//             "id": 1699421382313,
+//             "name": "Anita",
+//             "skills": [
+//                 {
+//                     "level": "SKILL_HIGH",
+//                     "count": 0
+//                 },
+//                 {
+//                     "level": "SKILL_MEDIUM",
+//                     "count": 1
+//                 },
+//                 {
+//                     "level": "SKILL_LOW",
+//                     "count": 0
+//                 }
+//             ]
+//         },
+//         {
+//             "id": 1699421402787,
+//             "name": "Satyam",
+//             "skills": [
+//                 {
+//                     "level": "SKILL_HIGH",
+//                     "count": 0
+//                 },
+//                 {
+//                     "level": "SKILL_MEDIUM",
+//                     "count": 0
+//                 },
+//                 {
+//                     "level": "SKILL_LOW",
+//                     "count": 0
+//                 }
+//             ]
+//         }
+//     ],
+//     "holidays": [
+//         {
+//             "id": 1699421434748,
+//             "startDate": "2023-11-10T08:00:00.000Z",
+//             "endDate": "2023-11-10T08:00:00.000Z",
+//             "name": "Dhanteras"
+//         }
+//     ],
+//     "vacations": [
+//         {
+//             "id": 1699421474334,
+//             "workerId": 1699421368835,
+//             "startDate": "2023-11-15T08:00:00.000Z",
+//             "endDate": "2023-11-15T08:00:00.000Z"
+//         },
+//         {
+//             "id": 1699421502577,
+//             "workerId": 1699421382313,
+//             "startDate": "2023-11-17T08:00:00.000Z",
+//             "endDate": "2023-11-20T08:00:00.000Z"
+//         }
+//     ]
+// }
